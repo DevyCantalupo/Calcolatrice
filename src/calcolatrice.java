b3376cf44ca7dfb20cfd481cda3a94ac63105933
Ronijs Barenitis <ronijs.barenitis@gmail.com>
2023-10-21 11:49:52 +0200

    Create 'evenOrOdd' function. Modify 'operator' function to work with the 'evenOrOdd function'

--- src/calcolatrice.java
+++ src/calcolatrice.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class calcolatrice {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
-        System.out.println("inserisci un operatore algebrico");
+        System.out.println("inserisci un operatore algebrico. Inserisci '?' per vedere se il numero è pari o dispari.");
         char operator =scan.next().charAt(0);
 
-        char[] validOperators = {'+', '-', '*', '/', '^'};
+        char[] validOperators = {'+', '-', '*', '/', '^', '?'};
 
 
         boolean isValidOperator = false;
@@ -25,11 +25,15 @@ public class calcolatrice {
     }
 
     public static void operator(char operator){
+        int secondNumber = 0;
         Scanner scanner = new Scanner(System.in);
         System.out.println("inserisci il  primo numero (o base per la potenza )");
         int firstNumber = scanner.nextInt();
-        System.out.println("inserisci il  seconda  numero (o di quanto elevare la base )");
-        int secondNumber = scanner.nextInt();
+        if (operator != '?'){
+            System.out.println("inserisci il  seconda  numero (o di quanto elevare la base )");
+            secondNumber = scanner.nextInt();
+        }
+
         scanner.close();
         switch(operator){
             case '+': System.out.println(addition(firstNumber,secondNumber));
@@ -42,6 +46,8 @@ public class calcolatrice {
                 break;
             case '^': System.out.println(power(firstNumber,secondNumber));
                 break;
+            case '?': System.out.println("Il numero è " +evenOrOdd(firstNumber));
+                break;
             default: System.out.println("input non valido");
         }
     }
@@ -53,4 +59,15 @@ public class calcolatrice {
     }
 
     public static int multiplication(int a, int b){ return a * b;}
+
+    public static String evenOrOdd(int a){
+        String result;
+        if(a%2 == 0){
+            result = "pari";
+        }
+        else {
+            result = "dispari";
+        }
+        return result;
+    }
 }
